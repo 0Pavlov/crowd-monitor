23d634480395759d155e9ec2113b954383c0cde3
0Pavlov <ffsvttvscsvb@gmail.com>
2025-05-01 16:25:31 +0300

    Create the database, check for success

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+from model import db_handler
+
+# Create/check the database
+created: int = db_handler.create_database("crowd.db")
+# Check success
+if not created:
+    exit("Database error.")
+
 
 app = Flask(__name__)
