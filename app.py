640f701e73014faddf658726402ad103bac617c2
0Pavlov <ffsvttvscsvb@gmail.com>
2025-05-01 16:31:37 +0300

    Fix the type hint typo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_session import Session
 from model import db_handler
 
 # Create/check the database
-created: int = db_handler.create_database("crowd.db")
+created: bool = db_handler.create_database("crowd.db")
 # Check success
 if not created:
     exit("Database error.")
