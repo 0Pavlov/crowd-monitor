9595ad2c29d43d1516e84de31507c2426e8ed753
0Pavlov <ffsvttvscsvb@gmail.com>
2025-05-02 20:08:40 +0300

    Configure session to use filesystem

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ if not created:
 app = Flask(__name__)
 
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
