8d3d9221a5519f0981e9221af78aa46af31c2d7b
0Pavlov <ffsvttvscsvb@gmail.com>
2025-05-02 20:24:44 +0300

    Create an apology() function which will render an apology.html template in case of an error

---
+++ helpers.py
@@ -0,0 +1,6 @@
+from flask import render_template
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    return render_template("apology.html", code=code, message=message)
