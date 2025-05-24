a3c62c41b3ac750a0dc0e835c3ba93d8af3b491a
0Pavlov <ffsvttvscsvb@gmail.com>
2025-05-24 23:22:19 +0300

    Rename and remove the old db_handler again

--- model/db_handler.py
+++ model/db_handler.py
@@ -147,7 +147,7 @@ def query(connection, query: str, *args):
         *args: Other arguments (mostly for ? placeholders).
     """
     try:
-        # Execute a query
+        # Ececute a query
         result = connection.execute(query, args)
         # Success output in console
         print(f"{GREEN}{query}{RESET}")
