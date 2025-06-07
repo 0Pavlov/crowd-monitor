f16ef80595bcfb54519268fef89c64a464574143
0Pavlov <ffsvttvscsvb@gmail.com>
2025-06-07 20:19:54 +0300

    Add the route decorator that checks if the user is logged in

--- helpers.py
+++ helpers.py
@@ -1,6 +1,23 @@
 from flask import render_template
+from functools import wraps
+from flask import redirect, session
 
 
 def apology(message, code=400):
     """Render message as an apology to user."""
     return render_template("apology.html", code=code, message=message)
+
+def login_required(f):
+    """
+    Require login in order to access the route.
+
+    This decorator wraps the route and ensures that only logged in users
+    can access the route.
+    """
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated
