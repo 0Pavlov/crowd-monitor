9ddeda389e73ce103fe54397558da8163b91f30d
0Pavlov <ffsvttvscsvb@gmail.com>
2025-06-10 14:32:14 +0300

    Add role validating logic to the validate_session decorator

--- app.py
+++ app.py
@@ -45,11 +45,13 @@ def validate_session(f):
         Or the current session is no longer valid because the user was deleted internally (users table entry).
         Checks if the user id is present in the users table.
         Checks if the username is stored in the session corresponds to that same id.
+        Checks if user's role matches the user's role in the db (prevent role manipulation).
         """
         # Get the session data
         try:
             user_id: int = session['user_id']
             username: str = session['username']
+            role: str = session['role']
         except:
             return apology("Invalid session. Unable to retrieve data from the session, session is corrupted.", code=400)
         
@@ -60,7 +62,7 @@ def validate_session(f):
 
         # Get the data for this username
         try:
-            data = db_handler.query(db, "SELECT username, id FROM users WHERE id = ?", user_id)
+            data = db_handler.query(db, "SELECT username, id, role FROM users WHERE id = ?", user_id)
         except:
             db.close()
             return apology("Invalid session. User no longer exists or the session is corrupted.", code=400)
@@ -76,6 +78,12 @@ def validate_session(f):
         if not valid:
             db.close()
             return apology("Invalid session. User's username doesn't match the internal user id. Session is corrupted.", code=400)
+
+        # Check if the user's session role is same as in the db
+        valid_role: bool = role == data[0]['role']
+        if not valid_role:
+            db.close()
+            return apology("Invalid session. Role mismatch.")
         
         db.close()
         return f(*args, **kwargs)
